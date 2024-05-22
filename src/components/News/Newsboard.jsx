54350792996ac0078ea4763a2a6dd6c4a6662cc7
anandjayan <anandudjayan29397@gmail.com>
2024-05-22 12:23:44 +0530

    er3

--- src/components/News/Newsboard.jsx
+++ src/components/News/Newsboard.jsx
@@ -7,7 +7,7 @@ const Newsboard = () => {
 
   useEffect(() => {
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${import.meta.env.VITE_API_KEY}`;
-    fetch(url).then(response=> response.json()).then(data=>setArticles(data.articles));
+    fetch(url).then(response=> response?.json()).then(data=>setArticles(data?.articles));
     
   }, [])
   
@@ -15,7 +15,7 @@ const Newsboard = () => {
     <div>
       <h2 className="text-center ">Latest <span className="badge bg-danger text-light">News</span></h2>
       {Articles?.map((news, index)=>{
-        return <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+        return <Newsitem key={index} title={news?.title} description={news?.description} src={news?.urlToImage} url={news?.url}/>
       })}
     </div>
   )
